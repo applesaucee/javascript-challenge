86aaf05a58cff2335e1a095ac0c0f5c70081675d
Tamanh <tamvn@uw.edu>
2014-10-25 15:32:31 -0700

    Added cancel capability. Needs work on clearing form field for occupation

--- js/signup.js
+++ js/signup.js
@@ -5,20 +5,44 @@
 
 "use strict";
 
-// loads the state select using the state data in the usStates global array
-// must create new option element for each state in the array
-// set the properties of that element
-// append new option element as child of the state select element
 function loadStates() {
 	var elem = document.getElementById("state");
+	
 	for (var i = 0; i < usStates.length; i++) {
 		var opt = document.createElement("option");
 		opt.value = usStates[i].code;
 		var text = document.createTextNode(usStates[i].name);
 		opt.appendChild(text);
+		// Why do we have to append text to option in order to show the text for the option?
+		// As in, what does it actually do?
 		elem.appendChild(opt);
-	}
-	
+	} 
 } // loadStates()
 
-document.addEventListener('DOMContentLoaded', loadStates);
+function showHideOcc() {
+	var occ = document.getElementsByName("occupationOther")[0];
+	
+	if (occupation.value == "other") {
+		occ.style.display = "inline";
+	} else {
+		occ.style.display = "none";
+		//occ.form.reset();
+		//need to figure out how to clear field without resetting the form the entire time
+	}
+} // showHideOcc()
+
+function leave() {
+	var depart = window.confirm("Do you really want to leave?");
+	
+	if (depart == true) {
+		window.location = 'http://google.com';
+	}
+} // leave()
+
+document.addEventListener('DOMContentLoaded', function() { 
+
+	loadStates();
+	occupation.addEventListener('change', showHideOcc);
+	cancelButton.addEventListener('click', leave);
+	//What allows us to access occupation without calling document.get.....?
+});
