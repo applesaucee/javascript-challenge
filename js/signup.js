fc989881570a9447258b6c12445d5f6da9fbde53
Tamanh <tamvn@uw.edu>
2014-10-25 12:43:35 -0700

    Added state id to index.html. State options work in signup.js

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,23 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+"use strict";
+
+// loads the state select using the state data in the usStates global array
+// must create new option element for each state in the array
+// set the properties of that element
+// append new option element as child of the state select element
+function loadStates() {
+	var elem = document.getElementById("state");
+	for (var i = 0; i < usStates.length; i++) {
+		var opt = document.createElement("option");
+		opt.value = usStates[i].code;
+		var text = document.createTextNode(usStates[i].name);
+		opt.appendChild(text);
+		elem.appendChild(opt);
+	}
+	
+} // loadStates()
+
+document.addEventListener('DOMContentLoaded', loadStates);
